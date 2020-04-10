6a0a5c152a911733ccc254af835e5c3ecb596ffa
ShaxromShukurov <59442362+Shaxrom@users.noreply.github.com>
2020-04-10 12:24:44 +0500

    Add files via upload

---
+++ ApiEx/src/main/java/SmsApi/repositories/SmsRepository.java
@@ -0,0 +1,7 @@
+package SmsApi.repositories;
+
+import SmsApi.models.SmsModels;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface SmsRepository extends JpaRepository <SmsModels, Long> {}
---
+++ ApiEx/src/test/java/BeelineService/ApiEx/ApiExApplicationTests.java
@@ -0,0 +1,13 @@
+package BeelineService.ApiEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
